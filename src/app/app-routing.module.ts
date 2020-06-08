5af2864323dcbfce061b2560530e2ef8f1d1e4c9
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-06-08 14:41:50 +0300

    1 file modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DuckListComponent } from '../duck-list/duck-list.component';
 import { ElephantListComponent } from '../elephant-list/elephant-list.component';
+import { PageNotFoundComponent} from '../page-not-found.component';
 
 const routes : Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: ''
+  },
   {path: 'ducks', component: DuckListComponent},
-  {path: 'elephants', component: ElephantListComponent}
+  {path: 'elephants', component: ElephantListComponent},
+  {path: "**", component: PageNotFoundComponent} 
 ];
 
 @NgModule({
@@ -14,4 +21,4 @@ const routes : Routes = [
   declarations: []
 })
 export class AppRoutingModule { }
-export const routingComponents = [DuckListComponent, ElephantListComponent]
+export const routingComponents = [DuckListComponent, ElephantListComponent, PageNotFoundComponent]
