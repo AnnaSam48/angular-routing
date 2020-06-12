6da98b21093a0406dae01c0aca04a5dcb473b415
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-06-12 12:03:46 +0300

    1 file modified

--- src/elephant-detail/elephant-detail.component.ts
+++ src/elephant-detail/elephant-detail.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-elephant-detail',
   template: `
   <h3>You selected elephant with id: {{elephantId}}</h3>
+  <a (click)="goPrevious()">Previous</a>
+  <a (click)="goNext()">Next</a>
   `,
   styles: []
 })
 export class ElephantDetailComponent implements OnInit {
 
   public elephantId;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
     let id= parseInt(this
