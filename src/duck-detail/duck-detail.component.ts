8df641e0415e73413f8bfe248a306f73a6dae226
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-06-15 11:28:11 +0300

    2 files modified

--- src/duck-detail/duck-detail.component.ts
+++ src/duck-detail/duck-detail.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-duck-detail',
   template: `
   <h3>You selected duck with id: {{duckId}}</h3>
+  <button (click)="goPrevious()">Previous</button>
+  <button (click)="goNext()">Next</button>
   `,
   styles: []
 })
 export class DuckDetailComponent implements OnInit {
 
   public duckId;
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-     let id= parseInt(this
+    this.route.paramMap.subscribe((params: ParamMap)=>{
+      let id = parseInt(params.get('id'));
+      this.duckId = id;
+    });
+    /* let id= parseInt(this
     .route
     .snapshot
     .paramMap
     .get('id'));
     this.duckId=id;
+    */
+  }
+
+   goPrevious(){
+    let previousId = this.duckId -1;
+      this.router.navigate(['/ducks', previousId ]);
+  }
+
+  goNext(){
+    let nextId = this.duckId +1;
+      this.router.navigate(['/ducks', nextId ]);
   }
 
 }
--- src/elephant-detail/elephant-detail.component.ts
+++ src/elephant-detail/elephant-detail.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-elephant-detail',
   template: `
   <h3>You selected elephant with id: {{elephantId}}</h3>
-  <a (click)="goPrevious()">Previous</a>
-  <a (click)="goNext()">Next</a>
+  <button (click)="goPrevious()">Previous</button>
+  <button (click)="goNext()">Next</button>
   `,
   styles: []
 })
@@ -16,12 +16,27 @@ export class ElephantDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
-    let id= parseInt(this
+    this.route.paramMap.subscribe((params: ParamMap)=>{
+      let id = parseInt(params.get('id'));
+      this.elephantId = id;
+    });
+    /*let id= parseInt(this
     .route
     .snapshot
     .paramMap
     .get('id'));
     this.elephantId=id;
+    */
+  }
+
+  goPrevious(){
+    let previousId = this.elephantId -1;
+      this.router.navigate(['/elephants', previousId ]);
+  }
+
+  goNext(){
+    let nextId = this.elephantId +1;
+      this.router.navigate(['/elephants', nextId ]);
   }
 
 }
